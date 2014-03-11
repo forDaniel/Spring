5e7bcc7cfb71200eae56ef6180530f4d07db7a98
dongyuan <dongyuan@ps-22e6a9.cttq.com>
2014-03-11 09:30:40 +0800

    init project

---
+++ Spring/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Spring
+//
+//  Created by dongyuan on 14-3-11.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
